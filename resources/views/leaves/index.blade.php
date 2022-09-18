c6709d3ead73162b2d96ecf385898c6a574bc115
majzoub.malek <majzoub.malek@gmail.com>
2022-09-18 10:45:45 +0300

    added rejection reason to leave request reject modal

--- resources/views/leaves/index.blade.php
+++ resources/views/leaves/index.blade.php
@@ -108,31 +108,35 @@
                                         </div>
                                     </div>
                                     <!-- Modal body -->
-                                    <div class="p-6 space-y-6">
-                                        <div class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                            Are you sure you want to reject this leave request? This action cannot be undone.
+                                    <form method="POST"
+                                          action="{{ route('leaves.reject', ['leave' => $leave->id]) }}">
+                                        @csrf
+                                        <div class="p-6 space-y-6">
+                                            <div class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                                Are you sure you want to reject this leave request? This action cannot be undone.
+                                            </div>
+                                            <div class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                                <label for="cancellation_reason" class="text-lg text-gray-600">Rejection Reason</label>
+                                                <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white rounded" name="cancellation_reason" ></textarea>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <!-- Modal footer -->
-                                    <div
-                                        class="flex justify-end items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                                        <div>
-                                            <button data-modal-toggle="rejectModal-{{$leave->id}}" type="button"
-                                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                                                Cancel
-                                            </button>
-                                        </div>
-                                        <div>
-                                            <form method="POST"
-                                                  action="{{ route('leaves.reject', ['leave' => $leave->id]) }}">
-                                                @csrf
+                                        <!-- Modal footer -->
+                                        <div
+                                            class="flex justify-end items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                                            <div>
+                                                <button data-modal-toggle="rejectModal-{{$leave->id}}" type="button"
+                                                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                                                    Cancel
+                                                </button>
+                                            </div>
+                                            <div>
                                                 <button data-modal-toggle="rejectModal-{{$leave->id}}"
                                                         class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                                                     Reject
                                                 </button>
-                                            </form>
+                                            </div>
                                         </div>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
