e5b8cc531c159bc3794d5100a7ea7e1f710b0ba5
majzoub.malek <majzoub.malek@gmail.com>
2022-09-20 19:04:38 +0300

    redirect to login page if user not logged in

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -63,7 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'role' => RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
         'disable_back' => \App\Http\Middleware\PreventBrowserBackHistory::class,
---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+
+class RoleMiddleware extends \Spatie\Permission\Middlewares\RoleMiddleware
+{
+    public function handle($request, Closure $next, $role, $guard = null)
+    {
+        $authGuard = Auth::guard($guard);
+
+        if ($authGuard->guest()) {
+            return redirect()->route('login');
+        }
+
+        $roles = is_array($role)
+            ? $role
+            : explode('|', $role);
+
+        if (! $authGuard->user()->hasAnyRole($roles)) {
+            throw UnauthorizedException::forRoles($roles);
+        }
+
+        return $next($request);
+    }
+}
