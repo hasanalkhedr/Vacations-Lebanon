ce8d3a01c3f97c7203b0cdf13b3c1a501db70874
Abed Nahouli <abednahouli@gmail.com>
2022-09-24 14:59:45 +0300

    fixed calendar day boxes

--- resources/views/leaves/calendar.blade.php
+++ resources/views/leaves/calendar.blade.php
@@ -17,33 +17,33 @@
         <tbody>
             @foreach ($employees as $employee)
                 <tr class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="border px-2 py-4 sm:text-sm font-bold text-gray-900 whitespace-nowrap dark:text-white">
+                    <td class="border px-2 py-3 sm:text-sm font-bold text-gray-900 whitespace-nowrap dark:text-white">
                         <div>
                             {{ $employee->first_name }} {{ $employee->last_name }}
                         </div>
                     </td>
                     @foreach($dates as $date)
                         @if(in_array($date->format('Y-m-d'), $weekends))
-                            <td class="aspect-square border border-b py-4 px-2 text-gray-900 bg-gray-500 whitespace-nowrap dark:text-white">
+                            <td class="aspect-square border border-b py-2 px-5 text-gray-900 bg-gray-500 whitespace-nowrap dark:text-white w-60">
                             </td>
                         @elseif(array_key_exists($employee->id . '&' . $date->format('Y-m-d'), $leaveId_dates_pairs))
                             @if($leaveId_dates_pairs[$employee->id . '&' . $date->format('Y-m-d')]->employee->id == $employee->id)
                                 @if($leaveId_dates_pairs[$employee->id . '&' . $date->format('Y-m-d')]->leave_duration->name == "Half Day AM")
-                                    <td class="aspect-square border border-b py-4 px-2 text-gray-900 bg-red-500 whitespace-nowrap dark:text-white">
+                                    <td class="aspect-square border border-b py-2 px-5 text-gray-900 bg-red-500 whitespace-nowrap dark:text-white w-60">
                                     </td>
                                 @elseif($leaveId_dates_pairs[$employee->id . '&' . $date->format('Y-m-d')]->leave_duration->name == "Half Day PM")
-                                    <td class="aspect-square border border-b py-4 px-2 text-gray-900 bg-blue-600 whitespace-nowrap dark:text-white">
+                                    <td class="aspect-square border border-b py-2 px-5 text-gray-900 bg-blue-600 whitespace-nowrap dark:text-white w-60">
                                     </td>
                                 @else
-                                    <td class="aspect-square border border-b py-4 px-2 text-gray-900 bg-yellow-600 whitespace-nowrap dark:text-white">
+                                    <td class="aspect-square border border-b py-2 px-5 text-gray-900 bg-yellow-600 whitespace-nowrap dark:text-white w-60">
                                     </td>
                                 @endif
                             @else
-                                <td class="border py-4 px-2 text-gray-900 whitespace-nowrap dark:text-white">
+                                <td class="border py-2 px-5 text-gray-900 whitespace-nowrap dark:text-white w-60">
                                 </td>
                             @endif
                         @else
-                            <td class="border py-4 px-2 text-gray-900 whitespace-nowrap dark:text-white">
+                            <td class="border py-2 px-5 text-gray-900 whitespace-nowrap dark:text-white w-60">
                             </td>
                         @endif
                     @endforeach
