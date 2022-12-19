66ed61a13d7fc2deb5e76331a3f088b9962bf16a
Abed Nahouli <abednahouli@gmail.com>
2022-12-19 23:39:59 +0200

    fix select option

--- resources/views/vendor/megaphone/admin/create-announcement.blade.php
+++ resources/views/vendor/megaphone/admin/create-announcement.blade.php
@@ -13,7 +13,7 @@
             <label for="type" class="block mb-2 text-sm font-medium blue-color">{{ __('Type') }}*</label>
             <select id="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
                 @error('type') border-red-500 @enderror" wire:model="type">
-                <option>{{ __('Select Type') }}</option>
+                <option value="" disabled selected>{{ __('Select Type') }}</option>
                 @foreach ($notifTypes as $type => $name)
                     <option value="{{ $type }}">
                         {{ $name }}
