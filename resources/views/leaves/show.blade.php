e3d50941ce49139bfa0fb1c672c2f13f5bd57128
Abed Nahouli <abednahouli@gmail.com>
2023-01-02 19:34:25 +0200

    fix attempt for carbon

--- resources/views/leaves/show.blade.php
+++ resources/views/leaves/show.blade.php
@@ -22,13 +22,13 @@
         </div>
         <div class="grid md:grid-cols-2 md:gap-6">
             <div class="relative z-0 mb-6 w-full group">
-                <input type="text" name="from" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" disabled value="{{ Carbon::createFromFormat('Y-m-d', $leave->from)->format('d/m/Y') }}" />
+                <input type="text" name="from" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" disabled value="{{ \Carbon\Carbon::createFromFormat('Y-m-d', $leave->from)->format('d/m/Y') }}" />
                 <label for="from" class="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 blue-color">
                     {{__("From")}}
                 </label>
             </div>
             <div class="relative z-0 mb-6 w-full group">
-                <input type="text" name="to" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" disabled value="{{ Carbon::createFromFormat('Y-m-d', $leave->to)->format('d/m/Y') }}"/>
+                <input type="text" name="to" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" disabled value="{{ \Carbon\Carbon::createFromFormat('Y-m-d', $leave->to)->format('d/m/Y') }}"/>
                 <label for="to" class="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 blue-color">
                     {{__("To")}}
                 </label>
