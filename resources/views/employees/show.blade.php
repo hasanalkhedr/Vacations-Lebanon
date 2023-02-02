ca04934ac5a83741e6b50ab746130607e234a4e8
majzoub.malek <majzoub.malek@gmail.com>
2023-02-02 23:00:54 +0200

    add step=0.5 to nb of days and confesisonnels inputs

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -291,7 +291,8 @@
                                     <div class="relative z-0 mb-4 w-full group">
                                         <input type="number" name="nb_of_days"
                                             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                            value="{{ $employee->nb_of_days }}" required />
+                                            value="{{ $employee->nb_of_days }}"
+                                               step="0.5" required />
                                         <label for="nb_of_days"
                                             class="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 blue-color">
                                             {{ __('Number of Days Off') }}
@@ -300,7 +301,8 @@
                                     <div class="relative z-0 mb-4 w-full group">
                                         <input type="number" name="confessionnels"
                                             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                            value="{{ $employee->confessionnels }}" required />
+                                            value="{{ $employee->confessionnels }}"
+                                            step="0.5" required />
                                         <label for="nb_of_days"
                                             class="peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 blue-color">
                                             {{ __('Confessionnel Days') }}
